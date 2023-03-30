fe9b594ea92eeb09bbea89407a5ba2dfa569aa1c
CptGrave <lukastrevo@gmail.com>
2023-03-30 12:29:19 +0200

    add sidebar adjustments

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -25,7 +25,7 @@ export default function Sidebar(props) {
     return (
         <section className="pane sidebar">
             <div className="sidebar--header">
-                <h3>Notes</h3>
+                <h3>Your notes</h3>
                 <button className="new-note" onClick={props.newNote}>+</button>
             </div>
             {noteElements}
